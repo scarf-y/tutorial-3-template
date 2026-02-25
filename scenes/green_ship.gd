035fee8f3e9f5b94d1d38d791da28740be2650c5
daf <daffa199naufal@gmail.com>
2026-02-25 19:56:02 +0700

    fix: reformatting green_ship.gd

--- scenes/green_ship.gd
+++ scenes/green_ship.gd
@@ -1,21 +1,22 @@
 extends CharacterBody2D
 
-@export var gravity = 200.0
-@export var walk_speed = 200
-@export var jump_speed = -300
+@export var gravity: float = 200.0
+@export var walk_speed: float = 200.0
+@export var jump_speed: float = -300.0
 
-func _physics_process(delta):
-	velocity.y += delta * gravity
-	
-	if is_on_floor() and Input.is_action_just_pressed('ui_up'):
+
+func _physics_process(delta: float) -> void:
+	velocity.y += gravity * delta
+
+	if is_on_floor() and Input.is_action_just_pressed("ui_up"):
 		velocity.y = jump_speed
-		
+
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -walk_speed
 	elif Input.is_action_pressed("ui_right"):
-		velocity.x =  walk_speed
+		velocity.x = walk_speed
 	else:
-		velocity.x = 0
+		velocity.x = 0.0
 
-	# "move_and_slide" already takes delta time into account.
+	# move_and_slide() already takes delta into account
 	move_and_slide()
